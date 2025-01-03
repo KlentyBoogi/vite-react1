d15efc8e8f021118ccb2f9a1d7e1b64c8bd7f729
Bhuvanesh Ram M <bhuvanesh@klenty.com>
2025-01-03 11:31:12 +0530

    Adding script - elevenlabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://elevenlabs.io/convai-widget/index.js';
+    script.async = true;
+    script.type = 'text/javascript';
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div>
       <h1>My App</h1>
-      {/* Add the custom element */}
       <elevenlabs-convai agent-id="7hTgY55DzvBMcGR4RBdX"></elevenlabs-convai>
     </div>
   );
